25686d4a744a792029f1ad410044a5f41c74656b
kholif18 <rahmadnur.kholifatur@gmail.com>
2026-05-09 12:03:06 +0700

    ptah theme fix

--- core/utils/loader.py
+++ core/utils/loader.py
@@ -1,9 +1,17 @@
 from core.utils.resource import resource_path
 
+
 def load_stylesheet(filename):
-    path = resource_path(f"apps/desktop/styles/{filename}")
 
-    if not path.exists():
-        raise FileNotFoundError(f"Style not found: {path}")
+    file_path = resource_path(
+        f"apps/desktop/styles/{filename}"
+    )
 
-    return path.read_text(encoding="utf-8")
+    print("STYLE PATH:", file_path)
+
+    if not file_path.exists():
+        raise FileNotFoundError(
+            f"Style not found: {file_path}"
+        )
+
+    return file_path.read_text(encoding="utf-8")
--- core/utils/resource.py
+++ core/utils/resource.py
@@ -1,7 +1,13 @@
 import sys
 from pathlib import Path
 
+
 def resource_path(relative_path):
+    """
+    Support dev + PyInstaller
+    """
+
     if hasattr(sys, "_MEIPASS"):
         return Path(sys._MEIPASS) / relative_path
+
     return Path(relative_path)
