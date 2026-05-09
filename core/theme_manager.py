2816b071a478b689971f28c628eee52872e62f88
kholif18 <rahmadnur.kholifatur@gmail.com>
2026-05-09 12:03:19 +0700

    theme path

--- core/theme_manager.py
+++ core/theme_manager.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QApplication
 from core.styles.loader import load_stylesheet
 
+
 class ThemeManager:
     def __init__(self):
         self.current_theme = "dark"
@@ -8,16 +9,26 @@ class ThemeManager:
     def apply(self, theme="dark"):
         self.current_theme = theme
 
-        file = "main_dark.qss" if theme == "dark" else "main_light.qss"
+        file = (
+            "main_dark.qss"
+            if theme == "dark"
+            else "main_light.qss"
+        )
 
-        # ❗ FIX: jangan kirim path manual lagi
         style = load_stylesheet(file)
 
         app = QApplication.instance()
+
         if app:
             app.setStyleSheet(style)
 
     def toggle(self):
-        new_theme = "light" if self.current_theme == "dark" else "dark"
+        new_theme = (
+            "light"
+            if self.current_theme == "dark"
+            else "dark"
+        )
+
         self.apply(new_theme)
+
         return new_theme
